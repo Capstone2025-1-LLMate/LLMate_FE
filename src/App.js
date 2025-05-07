168cb36792003408a856adb2b068a1945eb25fb6
krknulsh <esm9837@gmail.com>
2025-05-07 15:00:30 +0900

    로그인 구현

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import './App.css';
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./Login/LoginPage";
+import SignUpPage from "./SignUp/SignUpPage";
 import OutputPage from './output/outputPage';
 import InputPage from './input/inputPage';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <OutputPage />
-      {/* <InputPage/> */}
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/output" element={<OutputPage />} /> */}
+          
+          {/* <Route path="/input" element={<InputPage />} /> */}
+        </Routes>
+        <OutputPage />
+        {/* <InputPage/> */}
+      </div>
+    </Router>
   );
 }
 
