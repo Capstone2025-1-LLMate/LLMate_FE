10e0d991a4d207c13b0af54d58d3d487908166bf
Lee Jeong Wook <leejunguk14@naver.com>
2025-05-28 18:41:11 +0900

    대소문자 login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./Login/loginpage";
+import LoginPage from "./login/loginpage";
 import MyPage from "./mypage/mypage";
 import OutputPage from './output/outputPage';
 import InputPage from './input/inputPage';
