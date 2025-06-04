f0a6b3434593ac981dafe96956e072df69aa86ca
krknulsh <esm9837@gmail.com>
2025-06-04 13:48:06 +0900

    missing add . & directory error

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./login/loginpage";
-import MyPage from "./mypage/mypage";
 import OutputPage from './output/outputPage';
 import InputPage from './input/inputPage';
-import ModifyPage from './modify/ModifyPage';
-import ServerTest from './ServerTest';
-
+import ModifyPage from './modify/modifyPage';
 import './App.css';
 
 function App() {
@@ -15,16 +12,13 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<LoginPage/>} />
+          <Route path="/" element={<LoginPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/mypage" element={<MyPage />} />
           <Route path="/output" element={<OutputPage />} />
           <Route path="/input" element={<InputPage />} />
           <Route path="/modify"element={<ModifyPage />} />
-          <Route path="/stest" element={<ServerTest />} />
 
         </Routes>
-
         {/* <OutputPage /> */}
         {/* <InputPage/> */}
         {/* <ModifyPage/> */}
