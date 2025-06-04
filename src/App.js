2bcddfd0df35cfcce11c55611fae7b5c4ad3b5ae
Lee Jeong Wook <leejunguk14@naver.com>
2025-06-04 18:51:25 +0900

    app.js 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./login/loginPage";
+import LoginPage from "./login/loginpage";
 import MyPage from "./mypage/mypage";
 import OutputPage from './output/outputPage';
 import InputPage from './input/inputPage';
-import ModifyPage from './modify/modifyPage';
+import ModifyPage from './modify/modifypage';
 import './App.css';
 
 function App() {
@@ -13,11 +13,12 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<MyPage />} />
+          <Route path="/" element={<LoginPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/output" element={<OutputPage />} />
           <Route path="/input" element={<InputPage />} />
           <Route path="/modify"element={<ModifyPage />} />
+          <Route path="/mypage" element={<MyPage />} />
 
         </Routes>
         {/* <OutputPage /> */}
